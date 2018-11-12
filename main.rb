fc6508a7755398dbf59076b261deeec4133c5b8b
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-11-12 22:27:57 +0100

    Add fps and try to optimize display

--- main.rb
+++ main.rb
@@ -17,14 +17,34 @@ LINE_WIDTH = WINDOW_SIZE / 400
 CIRCLE_RADIUS = WINDOW_SIZE / 200
 
 
-collisions = Hash.new
+"""collisions = Array.new(N) { Array.new(N) { nil } }
+is_displayed = Array.new(N) { Array.new(N) { false } }
+N.times do |i|
+    (i+1...N).each do |j|
+        collisions[i][j] = Circle.new(x: 0, y: 0, radius: CIRCLE_RADIUS, color: 'red')
+        collisions[i][j].remove
+    end
+end"""
+
+collisions = []
+
 field = Field.new(N)
 lines = Array.new(N) { Line.new(width: LINE_WIDTH, color: 'white') }
 
-background = Rectangle.new(x: 0, y:0, width:80, height: 14, color:'silver', z: 10)
+background = Rectangle.new(x: 0, y:0, width:80, height: 26, color:'silver', z: 10)
 text_collisions = Text.new("", x: 2, y:2, size:10, color:'black', z: 11)
+text_fps = Text.new("FPS: ", x: 2, y: 14, size:10, color:'black', z: 11)
+
+last_fps = Time.now.to_f
+frames = 0
 
 update do
+    now = Time.now.to_f
+    if now > last_fps + 1
+        text_fps.text = "FPS: #{frames}"
+        frames = 0
+        last_fps = now
+    end
     # Movement and display
     field.length.times do |k|
         segment = field[k]
@@ -50,11 +70,12 @@ update do
     end
     # Collision detection
     n_collisions = 0
+    collisions.each(&:remove)
+    collisions = []
     (lines.length - 1).times do |i|
         abx = lines[i].x2 - lines[i].x1
         aby = lines[i].y2 - lines[i].y1
         (i+1...lines.length).each do |j|
-            key = [i, j]
 
             acx = lines[j].x1 - lines[i].x1
             acy = lines[j].y1 - lines[i].y1
@@ -65,7 +86,11 @@ update do
             beta = abx * ady - aby * adx
 
             if alpha < 0 == beta < 0
-                collisions.delete key
+                """collisions[i][j].remove
+                if is_displayed[i][j]
+                    is_displayed[i][j] = false
+                end"""
+                next
             end
 
             cdx = lines[j].x1 - lines[j].x2
@@ -79,7 +104,11 @@ update do
             beta = cdx * cby - cdy * cbx
 
             if alpha < 0 == beta < 0
-                collisions.delete key
+                """collisions[i][j].remove
+                if is_displayed[i][j]
+                    is_displayed[i][j] = false
+                end"""
+                next
             end
 
             denominator = alpha - beta
@@ -87,17 +116,16 @@ update do
             collisionx = (alpha * lines[i].x2 - beta * lines[i].x1) / denominator
             collisiony = (alpha * lines[i].y2 - beta * lines[i].y1) / denominator
 
-            if collisions.has_key? key
-                collisions[key].x = collisionx
-                collisions[key].y = collisiony
-            else
-                collisions[key] = Circle.new(x: collisionx, y: collisiony, radius: CIRCLE_RADIUS, color: "red")
-            end
+            """collisions[i][j].add unless is_displayed[i][j]
+            collisions[i][j].x = collisionx
+            collisions[i][j].y = collisiony"""
 
-            n_collisions += 1
+            collisions << Circle.new(x: collisionx, y: collisiony, radius: CIRCLE_RADIUS, color: "red")
+
         end
-        text_collisions.text = "Collisions: #{n_collisions}"
     end
+    text_collisions.text = "Collisions: #{collisions.length}"
+    frames += 1
 end
 
 show
